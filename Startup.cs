af1a8f94521e9e6c9cbc799a6b27a7bd15f8bc2c
Henrique Marques <hfvmarques@gmail.com>
2021-10-07 21:53:10 -0400

    added healthchecks endpoints /live and /ready for the api and mongo

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using BookCatalog.Repositores;
 using BookCatalog.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -33,11 +37,11 @@ namespace BookCatalog
     public void ConfigureServices(IServiceCollection services)
     {
       BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+      var mongoDBSettings = Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
 
       services.AddSingleton<IMongoClient>(ServiceProvider =>
       {
-        var settings = Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
-        return new MongoClient(settings.ConnectionString);
+        return new MongoClient(mongoDBSettings.ConnectionString);
       });
 
       services.AddSingleton<IBooksRepository, MongoDBBooksRepository>();
@@ -50,6 +54,13 @@ namespace BookCatalog
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookCatalog", Version = "v1" });
       });
+
+      services.AddHealthChecks()
+      .AddMongoDb(
+        mongoDBSettings.ConnectionString,
+        name: "mongodb",
+        timeout: TimeSpan.FromSeconds(3),
+        tags: new[] { "ready" });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +82,35 @@ namespace BookCatalog
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
+
+        endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+        {
+          Predicate = (check) => check.Tags.Contains("ready"),
+          ResponseWriter = async (context, report) =>
+          {
+            var result = JsonSerializer.Serialize(
+              new
+              {
+                status = report.Status.ToString(),
+                checks = report.Entries.Select(entry => new
+                {
+                  name = entry.Key,
+                  status = entry.Value.Status.ToString(),
+                  exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                  duration = entry.Value.Duration.ToString()
+                })
+              }
+            );
+
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+          }
+        });
+
+        endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+        {
+          Predicate = (_) => false
+        });
       });
     }
   }
