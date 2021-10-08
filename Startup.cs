737c5875b0d9928b7c790eb7ce3511f97ed23544
Henrique Marques <hfvmarques@gmail.com>
2021-10-07 22:53:00 -0400

    created docker image

--- Startup.cs
+++ Startup.cs
@@ -73,7 +73,10 @@ namespace BookCatalog
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookCatalog v1"));
       }
 
-      app.UseHttpsRedirection();
+      if (env.IsDevelopment())
+      {
+        app.UseHttpsRedirection();
+      }
 
       app.UseRouting();
 
